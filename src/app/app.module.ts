404afa894765f614adf021623974bffa24ea081e
CyrielleCarrieu <cyrielle.carrieu@gmail.com>
2020-01-31 10:48:28 +0100

    Ajout du bouton retour et des checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { HighSchoolComponent } from './pages/high-school/high-school.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { EditableElementComponent } from './shared/components/editable-element/editable-element.component';
 import { AboutPicturesFormDialogComponent } from './shared/dialogs/about-pictures-form-dialog/about-pictures-form-dialog.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -110,6 +111,7 @@ import { AboutPicturesFormDialogComponent } from './shared/dialogs/about-picture
     SlideshowModule,
     MatTabsModule,
     FlexLayoutModule,
+    MatCheckboxModule,
   ],
   providers: [
     {
