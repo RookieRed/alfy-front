567d5cd1d9c6e71f58d496203776a04e9f127a18
Lucia ALonso <lucia.alonso.duran@gmail.com>
2020-03-01 17:32:21 +0100

    squeltte accueil avec flex

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -4,12 +4,6 @@ import {PageService} from '../../services/page.service';
 import {PageInfo} from '../../models/page';
 import {environment} from '../../../environments/environment';
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 
 @Component({
   selector: 'app-about',
@@ -17,14 +11,6 @@ export interface Tile {
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  /*tiles: Tile[] = [
-    {text: 'Carousel', cols: 4, rows: 1, color: 'lightblue'},
-    {text: 'Agenda', cols: 1, rows: 3, color: 'lightgreen'},
-    {text: 'Presentation', cols: 3, rows: 1, color: 'lightpink'},
-    {text: 'Actualites', cols: 3, rows: 1, color: '#DDBDF1'},
-	  {text: 'Partenaires', cols: 3, rows: 1, color: 'lightblue'},
-  ];*/
 
   backgroundImages: (string)[] = [
     "/assets/img/logo-alfy.jpg",
