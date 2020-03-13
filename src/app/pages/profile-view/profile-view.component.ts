7bf3887d5d145d87c1a178b57f4924983458c320
Lucia ALonso <lucia.alonso.duran@gmail.com>
2020-03-13 11:29:52 +0100

    profile view parcours et exp a faire

--- src/app/pages/profile-view/profile-view.component.ts
+++ src/app/pages/profile-view/profile-view.component.ts
@@ -10,7 +10,13 @@ export class ProfileViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+    document.body.style.background = "url('../../../assets/img/connection-background.jpg') no-repeat 0 0";
+    document.body.style.backgroundSize = "auto";
+  }
+
+  ngOnDestroy() {
+    document.body.style.backgroundSize = "";
+    document.body.style.background = "";
   }
 
 }
