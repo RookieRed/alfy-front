1043727643cb6dcf1a150c46c3ab8f2757b00998
Lucia ALonso <lucia.alonso.duran@gmail.com>
2020-03-26 12:23:50 +0100

    recuperation donnes profile pour profilview

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http: HttpClient) {
+   }
+
+  public getUser() {
+    return this.http.get(environment.apiURL + '/account/me' ).toPromise();
+  }
+
+}
