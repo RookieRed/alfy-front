01c9cafdcf6e70629ad8dc639bb9e0939a0d79c2
Parisping <37449901+Parisping@users.noreply.github.com>
2020-04-02 09:57:03 +0200

    Fix user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -7,7 +7,7 @@ import {Project} from "./project";
 export class User {
 
   id: number;
-  userName: string;
+  username: string;
   firstName: string;
   lastName: string;
   birthDay: Date | Moment;
@@ -25,27 +25,31 @@ export class User {
   jobTitle: string;
   projects: Project[];
   address: Address;
+  password: string;
 
 
-  constructor(obj?: {id: number, userName: string, firstName: string, lastName: string, birthDay: Date | Moment, email: string, phone: string, role: string, profilePicture: File, coverPicture: File, facebook: string, linkedIn: string, twitter: string, instagram: string, baccalaureate: string, studies: Study[], jobTitle: string, projects: Project[], address: Address}) {
-    this.id = obj.id;
-    this.userName = obj.userName;
-    this.firstName = obj.firstName;
-    this.lastName = obj.lastName;
-    this.birthDay = obj.birthDay;
-    this.email = obj.email;
-    this.phone = obj.phone;
-    this.role = obj.role;
-    this.profilePicture = obj.profilePicture;
-    this.coverPicture = obj.coverPicture;
-    this.facebook = obj.facebook;
-    this.linkedIn = obj.linkedIn;
-    this.twitter = obj.twitter;
-    this.instagram = obj.instagram;
-    this.baccalaureate = obj.baccalaureate;
-    this.studies = obj.studies;
-    this.jobTitle = obj.jobTitle;
-    this.projects = obj.projects;
-    this.address = obj.address;
+  constructor(obj?: {id: number, userName: string, firstName: string, lastName: string, birthDay: Date | Moment, email: string, phone: string, role: string, profilePicture: File, coverPicture: File, facebook: string, linkedIn: string, twitter: string, instagram: string, baccalaureate: string, studies: Study[], jobTitle: string, projects: Project[], address: Address, password: string}) {
+    if ( obj != null ) {
+      this.id = obj.id;
+      this.username = obj.userName;
+      this.firstName = obj.firstName;
+      this.lastName = obj.lastName;
+      this.birthDay = obj.birthDay;
+      this.email = obj.email;
+      this.phone = obj.phone;
+      this.role = obj.role;
+      this.profilePicture = obj.profilePicture;
+      this.coverPicture = obj.coverPicture;
+      this.facebook = obj.facebook;
+      this.linkedIn = obj.linkedIn;
+      this.twitter = obj.twitter;
+      this.instagram = obj.instagram;
+      this.baccalaureate = obj.baccalaureate;
+      this.studies = obj.studies;
+      this.jobTitle = obj.jobTitle;
+      this.projects = obj.projects;
+      this.address = obj.address;
+      this.password = obj.password;
+    }
   }
 }
