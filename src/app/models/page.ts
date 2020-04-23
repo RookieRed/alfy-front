b06f63c183c5928f049f341b0c5818a86d6eab9d
Parisping <37449901+Parisping@users.noreply.github.com>
2020-04-23 16:16:57 +0200

    Fix modele page

--- src/app/models/page.ts
+++ src/app/models/page.ts
@@ -2,6 +2,25 @@ import {User} from "./user";
 import {TimedWritableContent} from "./timed.writable.content";
 import {Section} from "./sections";
 
+
+export class PageInfo {
+  public title: string;
+  public contents: PageContent[];
+  public files: PageFile[];
+}
+
+export class PageContent {
+  public id: number;
+  public title: string;
+  public html: string;
+}
+
+export class PageFile {
+  public id: number;
+  public path: string;
+  public options: any;
+}
+
 export class Page extends TimedWritableContent {
   title: string;
   // Mapping { "code-section-a": SectionA, "code-section-b": SectionB, ... }
